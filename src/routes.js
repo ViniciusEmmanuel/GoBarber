6e0001d0d30a5c9c0d1d6144cc4b449dbd9ed23f
Vinicius Emmanuel dell <vinicius.teixeira.m@gmail.com>
2019-07-28 16:47:44 -0300

    listando Providers

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const multerConfig = require('./config/multer');
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
+const ProviderController = require('./app/controllers/ProviderController');
 const FileController = require('./app/controllers/FileController');
 const authMiddleware = require('./app/middlewates/auth');
 
@@ -16,6 +17,8 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
